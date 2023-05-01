ffcca6b10cee2e39c44beeee9316eea013c12815
Brandin Crockett <brandincrockett@gmail.com>
2023-04-30 22:18:41 -0700

    Completed Assignment 012

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,2 +1,34 @@
 public class SortingAlgorithms {
+
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Original array:");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
+        selectionSort(numbers);
+        System.out.println("\nSorted array:");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
 }
